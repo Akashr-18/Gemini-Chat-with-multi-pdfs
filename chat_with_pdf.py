34f7e350dc319201472d0f3b124b214d70f3dba6
Akashr-18 <akash.ravichandran18@gmail.com>
2024-01-19 22:02:35 +0530

    Chat file updated

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -36,7 +36,7 @@ def get_conversational_chain(knowledge, user_query):
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {knowledge}\n
+    Context: \n{knowledge}\n
     Question: \n{user_query}\n
     Answer:
     """
